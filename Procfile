f188d55beeab3a7ede0b52c239b83f764f7b13ac
KristianDsmith <>
2023-10-16 09:36:45 +0000

    heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn roundhouse_records.wsgi:application
--- webshop/settings.py
+++ webshop/settings.py
@@ -16,6 +16,9 @@ import os
 if os.path.exists('env.py'):
     import env
 
+import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,12 +121,19 @@ WSGI_APPLICATION = 'webshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 
 
 # Password validation
